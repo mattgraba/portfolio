a834506f4282bb7aba936d1a5caba01b9fc77b74
Matt Graba <matt.graba14@gmail.com>
2025-09-26 18:19:02 -0500

    fix(analytics): remove unused font
    - layout.tsx: remove unused Inter font (or apply inter.className to body)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "../components/ui/Navbar"
-import { Inter } from "next/font/google";
 import Script from "next/script";
 import AnalyticsTracker from "../components/AnalyticsTracker";
 
